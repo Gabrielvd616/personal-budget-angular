85390f13c51afa95825ccf1890329f7ef3e15b8e
Gabriel Van Dreel <gvandree@uncc.edu>
2023-09-13 15:53:26 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,25 +2,9 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const budget = require("./budget.json");
 
 app.use("/", express.static("public"));
-
-const budget = {
-  myBudget: [
-    {
-      title: "Eat out",
-      budget: 25,
-    },
-    {
-      title: "Rent",
-      budget: 275,
-    },
-    {
-      title: "Grocery",
-      budget: 110,
-    },
-  ],
-};
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
