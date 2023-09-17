bc16daf685c143fa0904ad29be55e395f9977fbe
Gabriel Van Dreel <gvandree@uncc.edu>
2023-09-17 11:48:13 -0400

    Implemented CORS

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 // Budget API
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3001;
 const budget = require("./budget.json");
 
-app.use("/", express.static("public"));
-
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
 
 app.get("/budget", (req, res) => {
   res.json(budget);
